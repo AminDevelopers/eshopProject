770ef8e23c3a8f7d5a6a5c92b42753f34537ed3a
amin <aminledragon@gmail.com>
2024-03-07 15:56:14 +0100

    added config file for image api + laast updated website + added hamburger menu  + added shopping data + fully reponsive

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,21 +1,27 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "../styles/navbar.module.css";
 
 export default function Navbar() {
   const { isLogged, userLogged } = useSelector((state) => state.auth);
+  const { showMenu, setShowMenu } = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="NavBar">
-      <div className={styles.hamburger}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
         <FontAwesomeIcon
           className={styles.hamburgerIcon}
           icon={faBars}
         ></FontAwesomeIcon>
       </div>
-      <nav className={styles.navbar}>
+      <nav className={`${styles.navbar} ${showMenu ? styles.showMenu : ""}`}>
         <ul className={styles.ul}>
           <Link href={"/shop"} className={styles.link}>
             shop
--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,9 +1,37 @@
+"use client";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import styles from "../styles/shop.module.css";
 
-export default async function Shop() {
+export default function Shop() {
+  const [data, setData] = useState([]);
+  const [maxPrice, setMaxPrice] = useState(10);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const recupData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/products"
+        );
+        const reponseData = await response.json();
+        setData(reponseData);
+      } catch (error) {
+        console.log(`Error fetching data: ${error}`);
+      }
+    };
+    recupData();
+  }, []);
+
+  const filteredData = data.filter(
+    (element) =>
+      element.price <= maxPrice &&
+      element.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className={styles.shop}>
       <div className={styles.shopInfo}>
@@ -25,6 +53,62 @@ export default async function Shop() {
             ></FontAwesomeIcon>
           </Link>
         </div>
+      </div>
+      <div className={styles.filtercontainer}>
+        <h2 className={styles.filterby}>filter by</h2>
+        <div className={styles.filters}>
+          <div className={styles.pricefilter}>
+            <div className={styles.flexprice}>
+              <input
+                min={10}
+                max={1000}
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(parseInt(e.target.value))}
+                type="range"
+                className={styles.pricing}
+              />
+              <p
+                style={{
+                  color: "white",
+                  textTransform: "capitalize",
+                  fontFamily: "Lato",
+                }}
+              >
+                max Price: ${maxPrice}
+              </p>
+            </div>
+          </div>
+          <input
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            type="text"
+            placeholder="Search.."
+            className={styles.search}
+          />
+        </div>
+      </div>
+      <div className={styles.cards}>
+        {filteredData.map((element) => {
+          return (
+            <div key={element.id} className={styles.card}>
+              <Image
+                width={350}
+                height={350}
+                className={styles.img}
+                src={element.category.image}
+                alt={element.title}
+              />
+              <div className={styles.cardContent}>
+                <h2 className={styles.cardTitle}> {element.title} </h2>
+                <p className={styles.cardText}> {element.description} </p>
+              </div>
+              <div className={styles.buttons}>
+                <h2 className={styles.cardPrice}> ${element.price} </h2>
+                <h2 className={styles.add}>add</h2>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
