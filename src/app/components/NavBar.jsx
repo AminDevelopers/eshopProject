5dba83467d80ecafae57fc7ffdff9c505ed57251
amin <aminledragon@gmail.com>
2024-04-03 16:26:04 +0200

    latest updated website + fixed issues

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useState } from "react";
@@ -7,20 +7,54 @@ import styles from "../styles/navbar.module.css";
 
 export default function Navbar() {
   const { isLogged, userLogged } = useSelector((state) => state.auth);
-  const { showMenu, setShowMenu } = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
   return (
-    <div className="NavBar">
+    <div className={styles.NavBar}>
       <div className={styles.hamburger} onClick={toggleMenu}>
-        <FontAwesomeIcon
-          className={styles.hamburgerIcon}
-          icon={faBars}
-        ></FontAwesomeIcon>
+        {showMenu ? (
+          <FontAwesomeIcon
+            className={styles.hamburgerIcon}
+            icon={faXmark}
+          ></FontAwesomeIcon>
+        ) : (
+          <FontAwesomeIcon
+            className={styles.hamburgerIcon}
+            icon={faBars}
+          ></FontAwesomeIcon>
+        )}
       </div>
+
+      <div
+        className={`${styles.overlay} ${showMenu ? styles.showOverlay : ""} `}
+      >
+        <ul
+          className={`${styles.menuOverlay} ${
+            showMenu ? styles.menuOpen : styles.menuOverlay
+          }`}
+        >
+          <li>
+            <Link passHref href={"/"} className={styles.menuLink}>
+              home
+            </Link>
+          </li>
+          <li>
+            <Link passHref href={"/shop"} className={styles.menuLink}>
+              shop
+            </Link>
+          </li>
+          <li>
+            <Link passHref href={"/login"} className={styles.menuLink}>
+              login
+            </Link>
+          </li>
+        </ul>
+      </div>
+
       <nav className={`${styles.navbar} ${showMenu ? styles.showMenu : ""}`}>
         <ul className={styles.ul}>
           <Link href={"/shop"} className={styles.link}>
